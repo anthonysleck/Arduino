dc9d13b190121a966b72c0efdf1182dda0093fe0
Anthony Sleck <anthony.sleck@gmail.com>
2019-07-09 10:54:46 -0500

    Create sketch-template.ino

---
+++ sketch-template/sketch-template.ino
@@ -0,0 +1,26 @@
+/*
+   sketch-template
+   Description:
+   -TBD
+   Notes:
+   -TBD
+   Components:
+   -TBD
+   Contact Info:
+   email - anthony.sleck@gmail.com
+   github - https://github.com/anthonysleck
+   Changelog:
+   0.0.1 - ars - new code
+*/
+
+//inlcudes
+
+//constants-variables
+
+void setup() {
+
+}
+
+void loop() {
+
+}
